4a0cd4a75d494a54205f1084d83115f97f75bf6f
s200070 <s20007@std.it-college.ac.jp>
2021-11-22 10:32:35 +0900

    はまかわのテスト

--- app/src/main/java/jp/ac/it_college/std/s20007/newquiz/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20007/newquiz/MainActivity.kt
@@ -2,10 +2,13 @@ package jp.ac.it_college.std.s20007.newquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s20007.newquiz.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
