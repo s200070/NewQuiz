f6b9625c4ae2ac31b14f0b9eba95ecccc112867a
s200070 <s20007@std.it-college.ac.jp>
2021-11-22 10:35:23 +0900

    はまかわのテスト2

--- app/src/main/java/jp/ac/it_college/std/s20007/newquiz/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20007/newquiz/MainActivity.kt
@@ -3,12 +3,16 @@ package jp.ac.it_college.std.s20007.newquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import jp.ac.it_college.std.s20007.newquiz.databinding.ActivityMain2Binding
+import jp.ac.it_college.std.s20007.newquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMain2Binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.startButton.text = "はまかわ"
     }
+
 }
