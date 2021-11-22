74c354ce8eed07466aed6c6be2aab48690f7518e
s200070 <s20007@std.it-college.ac.jp>
2021-11-22 10:24:53 +0900

    test

---
+++ app/src/main/java/jp/ac/it_college/std/s20007/newquiz/MainActivity2.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s20007.newquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
